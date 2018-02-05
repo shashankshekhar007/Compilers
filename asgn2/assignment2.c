2ba929aa52c0468fc8160bd350a2064dc6d98ec4
Shashank Shekhar <Shashank@Shashanks-MacBook-Air.local>
2018-02-05 11:40:24 +0530

    Starting assignment 2

---
+++ asgn2/assignment2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct each_line_of_file {
+	char line[100];
+} lines_of_a_file;
+
+int main(int argc, char* argv[]){
+	FILE* fp;
+	FILE*  copy_of_fp;
+	if (argc ==1){
+		FILE* fp;
+		fp = fopen(argv[0], "r");
+		copy_of_fp = fopen (argv[0], "r");
+	}
+	int number_of_lines=0;
+	if(fp == "NULL")
+		printf("Couldn't find the desired file\n");
+	char c;
+	for(c=getc(fp); c!=EOF; c= getc(fp)){
+		if(c=='\n')
+			number_of_lines++;
+		fclose(fp);
+	}
+	number_of_lines=10;
+	lines_of_a_file lines[number_of_lines];
+	printf("Number of lines = %d", number_of_lines); 
+	return 0;
+}
