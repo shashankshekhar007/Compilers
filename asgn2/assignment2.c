ed38d6952fceac070dc5ce90dadcad31308131b1
shashankshekhar007 <sshekhar@iitk.ac.in>
2018-02-06 19:52:04 +0530

    Symbol table done.

--- asgn2/assignment2.c
+++ asgn2/assignment2.c
@@ -22,6 +22,7 @@ typedef struct variable_list {
 	int nextuse;
 } VL;
 
+//get index of variable in variable list
 int get_variable_index(VL* variablelist, char* variable, int length){
 	int i;
 	for(i=0;i<length;i++){
@@ -30,6 +31,7 @@ int get_variable_index(VL* variablelist, char* variable, int length){
 	}
 }
 
+//copied from internet :p
 int cmpfnc (const void * a, const void * b) {
    return ( *(int*)a - *(int*)b );
 }
@@ -96,7 +98,8 @@ int main(int argc, char** argv){
         lines = (char **)malloc(number_of_lines*sizeof(char *));
 	char words[number_of_lines][10][10];
 
-        int i;
+
+        int i,j;
         for(int i=0;i<number_of_lines;i++){
             lines[i]= (char *)malloc(100*sizeof(char));
             number_of_words[i]=0;
@@ -134,37 +137,41 @@ int main(int argc, char** argv){
 		j++; l++;
     	    }
     	}
+    	free(lines);
+    	//lines ka kaam khatam. Ab isko free kar diya.
 
-    	//Now we move on to identifying the types of instructions.
-	int type_of_instruction[number_of_lines];
+    	VL variables[number_of_lines*3];
+	int number_of_variables=0;
+	int k;
 	for(i=0;i<number_of_lines;i++){
-		if(strcmp(words[i][0],"=")==0)
-			type_of_instruction[i]=BINARYASSIGNMENT;
-		else if(isOperator(words[i][0]))
-			type_of_instruction[i]=OPERATION;
-		else if(strcmp(words[i][0],"goto")==0)
-			type_of_instruction[i]=GOTO;
-		else if(strcmp(words[i][0],"ifgoto")==0)
-			type_of_instruction[i]=IFGOTO;
-		else if(strcmp(words[i][0],"return")==0)
-			type_of_instruction[i]=RETURN;
-		else if(strcmp(words[i][0],"label")==0)
-			type_of_instruction[i]=FUNCTIONDECLARATION;
-		else if(strcmp(words[i][0],"call")==0)
-			type_of_instruction[i]=FUNCTIONCALL;
-		else if(strcmp(words[i][0],"print")==0)
-			type_of_instruction[i]=PRINTSTATEMENT;
-		else
-			type_of_instruction[i]=ERROR;
+		for(j=0;j<number_of_words[i];j++){
+			int flag=1;
+			if(isKeyword(words[i][j]) || isNumber(words[i][j]) || isOperator(words[i][j])){
+				continue;
+			}
+			for(k=0;k<number_of_variables;k++){
+				if(strcmp(words[i][j],variables[k].name)==0)
+					flag=0;
+			}
+			if(flag==1){
+				strcpy(variables[number_of_variables++].name,words[i][j]);
+			}
+		}
 	}
-	/*for(i=0;i<number_of_lines;i++)
-		printf("%d\n",type_of_instruction[i]);
-	*/
-	//preprocessing has been done. Moving on to find headers.
-	int headers[number_of_lines];
+	for(i=0;i<number_of_variables;i++)
+		printf("%s\t",variables[i].name);
+	printf("\n");
+	for(i=0;i<number_of_variables;i++){
+		printf("%d ",get_variable_index(variables,variables[i].name,number_of_variables));
+	}
+	//variables identify ho gaye hain.
+
+	printf("\n");
+    	
+	//time to find out headers
+    	int headers[number_of_lines];
 	int header_count = 1;
 	headers[0]=0;
-	int j;
 	for(i=0;i<number_of_lines;i++){
 		if(strcmp(words[i][0],"goto")!=0 && strcmp(words[i][0],"ifgoto")!=0 && strcmp(words[i][0],"label")!=0){
 			continue;
@@ -202,46 +209,41 @@ int main(int argc, char** argv){
 	}
 	qsort(headers, header_count, sizeof(int), cmpfnc);
 	//all header lines have now been captured
-	/*for(i=0;i<number_of_lines;i++){
-		for(j=0;j<number_of_words[i];j++){
-			printf("%s\n", words[i][j]);
-		}
-	}*/
+	
 	for(i=0;i<header_count;i++)
 		printf("Headers = %d\n",headers[i]);
 
-	//moving on to the next part of variable listing.
-	VL variables[number_of_lines];
-	int number_of_variables=0;
-	int k;
+    	//Now we move on to identifying the types of instructions.
+	int type_of_instruction[number_of_lines];
 	for(i=0;i<number_of_lines;i++){
-		for(j=0;j<number_of_words[i];j++){
-			int flag=1;
-			if(isKeyword(words[i][j]) || isNumber(words[i][j]) || isOperator(words[i][j])){
-				continue;
-			}
-			for(k=0;k<number_of_variables;k++){
-				if(strcmp(words[i][j],variables[k].name)==0)
-					flag=0;
-			}
-			if(flag==1){
-				strcpy(variables[number_of_variables++].name,words[i][j]);
-			}
-		}
+		if(strcmp(words[i][0],"=")==0)
+			type_of_instruction[i]=BINARYASSIGNMENT;
+		else if(isOperator(words[i][0]))
+			type_of_instruction[i]=OPERATION;
+		else if(strcmp(words[i][0],"goto")==0)
+			type_of_instruction[i]=GOTO;
+		else if(strcmp(words[i][0],"ifgoto")==0)
+			type_of_instruction[i]=IFGOTO;
+		else if(strcmp(words[i][0],"return")==0)
+			type_of_instruction[i]=RETURN;
+		else if(strcmp(words[i][0],"label")==0)
+			type_of_instruction[i]=FUNCTIONDECLARATION;
+		else if(strcmp(words[i][0],"call")==0)
+			type_of_instruction[i]=FUNCTIONCALL;
+		else if(strcmp(words[i][0],"print")==0)
+			type_of_instruction[i]=PRINTSTATEMENT;
+		else
+			type_of_instruction[i]=ERROR;
 	}
-	for(i=0;i<number_of_variables;i++)
-		printf("%s\t",variables[i].name);
-	printf("\n");
-	for(i=0;i<number_of_variables;i++){
-		printf("%d ",get_variable_index(variables,variables[i].name,number_of_variables));
-		//printf("%s\t",variables[i].name);
-	}
-	printf("\n");
-	//variables are now being recognized. 
-	//printf("variable 2 = %s and variable 10 = %s",variables[2].name, variables[10].name);
-
-	//type of instructions are now being identified. Now we need to generate the next use table.
+	for(i=0;i<number_of_lines;i++)
+		printf("type_of_instruction of line %d\t%d\n",i,type_of_instruction[i]);
+	
+	//type of instructions are now being identified. Now we need to generate the next use table and symbol table.
 	VL symboltable[number_of_lines][number_of_variables];
+	printf("header_count = %d\n",header_count);
+	for(i=0;i<header_count;i++)
+		printf("Headers = %d\n",headers[i]);
+
 	for (i=0;i<number_of_lines;i++){
 		for(j=0;j<number_of_variables;j++){
 			strcpy(symboltable[i][j].name,variables[j].name);
@@ -249,6 +251,7 @@ int main(int argc, char** argv){
 			symboltable[i][j].nextuse = NONE;
 		}
 	}
+	
 	int line_number;
 	for(line_number=number_of_lines-1;line_number>headers[header_count-1];line_number--){
 		printf("At line number %d \n", line_number);
@@ -364,6 +367,7 @@ int main(int argc, char** argv){
 		symboltable[line_number][k].nextuse=symboltable[line_number+1][k].nextuse;
 	switch (type_of_instruction[line_number]){
 		case BINARYASSIGNMENT:
+		printf("Changing the status of variable number %d\n",get_variable_index(variables,words[line_number][1],number_of_variables));
 		symboltable[line_number][get_variable_index(variables,words[line_number][1],number_of_variables)].status=LIVE;
 		if(!isNumber(words[line_number][2])){
 			symboltable[line_number][get_variable_index(variables,words[line_number][2],number_of_variables)].nextuse=line_number;
